385ec0a8afc6cd0c816f4f5d8f79183afb04f7a7
IRUMVAEmmanuel1 <emmanuelirumva1@gmail.com>
2022-09-29 16:33:43 +0200

    error page changes

--- src/component/LandingPage/ErrorPage.js
+++ src/component/LandingPage/ErrorPage.js
@@ -2,7 +2,10 @@ import React from 'react'
 
 function ErrorPage() {
   return (
+    <>
     <div className='mt-20  text-center text-2xl text-green-400'>ERROR PAGE NOT FOUND!</div>
+    <div className='mt-20  text-center text-2xl text-green-400'>ERROR PAGE NOT FOUND!</div>
+    </>
   )
 }
 
